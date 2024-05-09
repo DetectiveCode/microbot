e510bad359bb71fc13a51b01584ff13fcbf93023
DetectiveCode <fabs1996@live.co.uk>
2024-05-09 12:45:40 +0800

    Fixed issue with username not appearing in the title bar.

--- runelite-client/src/main/java/net/runelite/client/ui/ClientUI.java
+++ runelite-client/src/main/java/net/runelite/client/ui/ClientUI.java
@@ -264,7 +264,7 @@ public class ClientUI
 				return false;
 			}
 
-			frame.setTitle(title);
+			frame.setTitle(title + " - " + name);
 			return true;
 		});
 	}
@@ -1142,7 +1142,7 @@ public class ClientUI
 
 			if (player != null && player.getName() != null)
 			{
-				frame.setTitle(title);
+				frame.setTitle(title + " - " + player.getName());
 			}
 		}
 		else
